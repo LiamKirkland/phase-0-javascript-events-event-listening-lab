dd0e0a01e0c37ab91ddcdc61d294cead94efd63f
LiamKirkland <liam.b.kirkland@gmail.com>
2026-05-12 14:11:57 -0400

    Event listeners practice

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+  const input = document.getElementById("button");
+  input.addEventListener("click", function () {
+    alert("I was clicked!");
+    console.log("clicked")
+  });
 }
+
+addingEventListener() // I was wondering why the button wasn't doing anything, realized it's cause the function was never called
